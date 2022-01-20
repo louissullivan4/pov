2b8b43df826285956341f2e0dd6fd769bfac4e77
Louis <louis.sullivan@umail.ucc.ie>
2022-01-20 18:51:36 +0000

    created twitter bot to get data and the sentiment analysis

---
+++ Backend/george.py
@@ -0,0 +1,53 @@
+from textblob import TextBlob
+
+class George:
+    def __init__(self, input):
+        self.data = input
+        self.total_sentiments = 0
+        self.total_positve = 0
+        self.total_negative = 0
+        self.total_neutral = 0
+        self.pol = 0
+        self.getSentiments()
+        self.result = self.calculateResults()
+
+    def getSentiments(self):
+        for val in self.data():
+            val = TextBlob(val)
+            for sentence in val.sentences:
+                polarity = float(sentence.sentiment.polarity)
+                self.pol = polarity
+                self.total_sentiments += 1
+                if polarity >= 0.1:
+                    self.total_positve += 1
+                elif polarity <= -0.1:
+                    self.total_negative += 1
+                else:
+                    self.total_neutral += 1
+    
+    def calculateResults(self):
+        return (self.total_positve / self.total_sentiments) * 100
+
+    def __str__(self):
+        return "Text Input: {} \nTotal Input: {} \nTotal Positive: {} \nTotal Negative: {} \nTotal Neutral: {} \nPolarity: {} \nPositive Rating: {}% \n".format(self.data, self.total_sentiments, self.total_positve, self.total_negative, self.total_neutral, self.pol, self.result)
+
+
+if __name__ == "__main__":
+    bot = George()
+    print(bot)
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+if __name__ == "__main__":
+    bot = George
---
+++ Backend/get_data.py
@@ -0,0 +1,37 @@
+import requests
+from config import config
+import tweepy as tw
+from george import George
+
+TWITTER_CLIENT = config.twittersecret
+TWITTER_KEY = config.twitterkey
+TWITTER_ACCESS_SECRET = config.twitteracceesssecret
+TWITTER_ACCESS_KEY = config.twitteraccess
+
+class TwitterData:
+    def __init__(self, term):
+        self.api = self.createApi()
+        self.term = term
+        self.tweets_list = []
+        self.result = self.analysis_tweets()
+
+    def createApi(self):
+        auth = tw.OAuthHandler(TWITTER_KEY, TWITTER_CLIENT)
+        auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
+        return tw.API(auth)
+
+    def getTweets(self):
+        for tweet in self.api.search_tweets(q=self.term,count=100,lang="en", since="2017-04-03").items():
+                self.tweets_list.append(tweet.text)
+
+    def analysis_tweets(self):
+        analysis = George(self.tweets_list)
+        return analysis
+
+    def __str__(self):
+        return "{}".format(self.result)
+
+
+if __name__ == "__main__":
+    bot = TwitterData()
+    print(bot)
