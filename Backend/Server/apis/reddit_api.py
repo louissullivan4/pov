87080a0769232f7fd2ba3c1a38c0deebc7dac83a
Louis <louis.sullivan@umail.ucc.ie>
2022-02-26 23:12:20 +0000

    filter post

--- Backend/Server/apis/reddit_api.py
+++ Backend/Server/apis/reddit_api.py
@@ -7,17 +7,26 @@ import pandas as pd
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 import praw
+import re
 from keys import *
 
-# maths imports
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import imp
-# from IPython import display
-# import math
-# import re
-# import seaborn as sns
-# sns.set(style='darkgrid', context='talk', palette='Dark2')
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+    u"\U0001F600-\U0001F64F" u"\U0001F680-\U0001F6FF" u"\U0001F1E0-\U0001F1FF" u"\U00002500-\U00002BEF"  
+    u"\U00002702-\U000027B0"u"\U00002702-\U000027B0"u"\U000024C2-\U0001F251"u"\U0001f926-\U0001f937"
+    u"\U00010000-\U0010ffff"u"\u2640-\u2642"u"\u2600-\u2B55"u"\u200d"u"\u23cf"u"\u23e9"u"\u231a"
+    u"\ufe0f"u"\u3030""]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+def filter_post(post):
+    newline_remove = post.replace("\n", " ")
+    rt_remove = re.compile('RT @').sub('@', newline_remove, count=1)
+    username_remove = re.sub('@[^\s]+', '', rt_remove)
+    filtered = username_remove.strip()
+    string_encode = filtered.encode("ascii", "ignore")
+    string_decode = string_encode.decode()
+    string_decode = " ".join(string_decode.split())
+    return string_decode
 
 def reddit_search(searchterm, category, id, secret, user):
     sia = SIA()
@@ -41,48 +50,34 @@ def reddit_search(searchterm, category, id, secret, user):
         json_out = {}
 
         for submission in resp:
-            headlines.add(submission.title)
-            # print(len(headlines))
+            post = remove_emoji(submission.title)
+            filtered_post = filter_post(post)
+            headlines.add(filtered_post)
 
         for line in headlines:
             pol_score = sia.polarity_scores(line)
             pol_score['headline'] = line
             results.append(pol_score)
 
-        # pprint(results[:3], width=100)
-
         df = pd.DataFrame.from_records(results)
         df['label']=0
-        df.loc[df['compound']> 0.2, 'label'] = 1 # positive
-        df.loc[df['compound']< -0.2, 'label'] = -1 # positive
+        df.loc[df['compound']> 0.2, 'label'] = 1
+        df.loc[df['compound']< -0.2, 'label'] = -1
         df.head()
-        # saving csv
         df2 = df[['headline', 'label']]
-        # df2.to_csv('reddit_headlines_labels.csv', mode='a', encoding='utf-8', index=False)
 
-        # print("Positive headlines:\n")
-        # pprint(list(df[df['label'] == 1].headline)[:5], width=200)
         json_out.update({'Positive headlines': list(df[df['label'] == 1].headline)[:5]})
-        # print("Negative headlines:\n")
-        # pprint(list(df[df['label'] == -1].headline)[:5], width=200)
         json_out.update({'Negative headlines': list(df[df['label'] == -1].headline)[:5]})
-        # print(df.label.value_counts())
+
         headline_count = df.label.value_counts()
         json_out.update({'Headline count': headline_count.to_json()})
-        # print(df.label.value_counts(normalize=True) * 100)
+
         headline_count_per = df.label.value_counts(normalize=True)
         json_out.update({'Headline count %': headline_count_per.to_json()})
 
-        # bar chart
-        # fig, ax = plt.subplots(figsize=(8, 8))
-        # counts = df.label.value_counts(normalize=True) * 100
-        # sns.barplot(x=counts.index, y=counts, ax=ax)
-        # ax.set_xticklabels(['Negative', 'Neutral', 'Positive'])
-        # ax.set_ylabel("Percentage")
-        # plt.show()
-
         tokenizer = RegexpTokenizer(r'\w+')
         stop_words = stopwords.words('english')
+
         # word distro
         def process_text(headlines):
             tokens = []
@@ -95,13 +90,11 @@ def reddit_search(searchterm, category, id, secret, user):
         pos_lines = list(df[df.label == 1].headline)
         pos_tokens = process_text(pos_lines)
         pos_freq = nltk.FreqDist(pos_tokens)
-        # print(pos_freq.most_common(20))
         json_out.update({'Positive word distro': pos_freq.most_common(20)})
 
         neg_lines = list(df2[df2.label == -1].headline)
         neg_tokens = process_text(neg_lines)
         neg_freq = nltk.FreqDist(neg_tokens)
-        # print(neg_freq.most_common(20))
         json_out.update({'Negative word distro': neg_freq.most_common(20)})
 
         dict_dump = json_out
@@ -148,4 +141,5 @@ def reddit_search(searchterm, category, id, secret, user):
         new_json.update({"msg": "Entry unavailable"})
     return(new_json)
 
-# print(reddit_search("adele", "music", client_id(), client_secret(), user_agent()))
+if __name__ == "__main__":
+    print(reddit_search("adele", "music", client_id(), client_secret(), user_agent()))
